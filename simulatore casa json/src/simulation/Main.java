02c3c08cdc6b76d28bb5cc4c394797baa9961972
Nicola-Lanzi <72873269@itiscastelli.onmicrosoft.com>
2026-02-10 11:35:46 +0100

    Update Casa initialization in Main.java

--- simulatore casa json/src/simulation/Main.java
+++ simulatore casa json/src/simulation/Main.java
@@ -1,8 +1,9 @@
 import simulation.*;
 
 void main() {
-        Casa casa1 = new Casa("nicola,diego,francesca,manuel");
+        Casa casa1 = new Casa("4DI");
         String fileJson = "config.json";
         Simulatore simulatore = new Simulatore();
         simulatore.esegui(casa1, fileJson);
 }
+
